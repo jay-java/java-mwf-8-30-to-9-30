75ed25cd07abe4fb394047a80466ee2ce8410183
jay-java <jayprajapati.tops@gmail.com>
2023-02-24 09:41:40 +0530

    profile change password

--- Project/src/main/java/Controller/SellerController.java
+++ Project/src/main/java/Controller/SellerController.java
@@ -6,6 +6,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.tomcat.util.scan.AbstractInputStreamJar;
 
 import Dao.SellerDao;
 import Model.Seller;
@@ -48,6 +51,55 @@ public class SellerController extends HttpServlet {
 			SellerDao.insertSeller(s);
 			request.setAttribute("msg", "Account registered succesfully");
 			request.getRequestDispatcher("seller-login.jsp").forward(request, response);
+		}
+		else if(action.equalsIgnoreCase("login")) {
+			Seller s = new Seller();
+			s.setEmail(request.getParameter("email"));
+			s.setPassword(request.getParameter("password"));
+			Seller s1 = SellerDao.loginSeller(s);
+			if(s1 == null) {
+				request.setAttribute("msg1", "password is incorrect");
+				request.getRequestDispatcher("seller-login.jsp").forward(request, response);
+			}
+			else {
+				HttpSession session = request.getSession();
+				session.setAttribute("data", s1);
+				request.getRequestDispatcher("seller-home.jsp").forward(request, response);
+			}
+		}
+		else if(action.equalsIgnoreCase("update")) {
+			Seller s = new Seller();
+			s.setId(Integer.parseInt(request.getParameter("id")));
+			s.setName(request.getParameter("name"));
+			s.setContact(Long.parseLong(request.getParameter("contact")));
+			s.setAddress(request.getParameter("address"));
+			s.setEmail(request.getParameter("email"));
+			SellerDao.updateProfile(s);
+			HttpSession session = request.getSession();
+			session.setAttribute("data", s);
+			request.getRequestDispatcher("seller-home.jsp").forward(request, response);
+		}
+		else if(action.equalsIgnoreCase("change password")) {
+			Seller s = new Seller();
+			int id = Integer.parseInt(request.getParameter("id"));
+			String op = request.getParameter("op");
+			String np = request.getParameter("np");
+			String cnp = request.getParameter("cnp");
+			boolean flag = SellerDao.checkOldPassword(id, op);
+			if(flag==true) {
+				if(np.equals(cnp)) {
+					SellerDao.changePasswrod(id, np);
+					response.sendRedirect("seller-home.jsp");
+				}
+				else {
+					request.setAttribute("msg1", "old password and new password not matched");
+					request.getRequestDispatcher("seller-change-password.jsp").forward(request, response);
+				}
+			}
+			else {
+				request.setAttribute("msg", "Old password incorrect");
+				request.getRequestDispatcher("seller-change-password.jsp").forward(request, response);
+			}
 		}
 	}
 
--- Project/src/main/java/Dao/SellerDao.java
+++ Project/src/main/java/Dao/SellerDao.java
@@ -1,7 +1,9 @@
 package Dao;
 
+import java.security.spec.RSAKeyGenParameterSpec;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import org.apache.tomcat.dbcp.dbcp2.PStmtKey;
 
@@ -21,6 +23,75 @@ public class SellerDao {
 			pst.setString(5, s.getPassword());
 			pst.executeUpdate();
 			System.out.println("data inserted");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static Seller loginSeller(Seller s) {
+		Seller s1 = null;
+		try {
+			Connection connection = DBConnection.createConnection();
+			String sqlString="select * from seller where email=? and password=?";
+			PreparedStatement pst = connection.prepareStatement(sqlString);
+			pst.setString(1, s.getEmail());
+			pst.setString(2, s.getPassword());
+			ResultSet rSet = pst.executeQuery();
+			if(rSet.next()) {
+				s1 = new Seller();
+				s1.setId(rSet.getInt("id"));
+				s1.setName(rSet.getString("name"));
+				s1.setContact(rSet.getLong("contact"));
+				s1.setAddress(rSet.getString("address"));
+				s1.setEmail(rSet.getString("email"));
+				s1.setPassword(rSet.getString("password"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return s1;
+	}
+	public static void updateProfile(Seller s) {
+		try {
+			Connection connection = DBConnection.createConnection();
+			String sqlString="update seller set name=?,contact=?,address=?,email=? where id=?";
+			PreparedStatement pst = connection.prepareStatement(sqlString);
+			pst.setString(1, s.getName());
+			pst.setLong(2, s.getContact());
+			pst.setString(3, s.getAddress());
+			pst.setString(4, s.getEmail());
+			pst.setInt(5, s.getId());
+			pst.executeUpdate();
+			System.out.println("data updated");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static boolean checkOldPassword(int id,String op) {
+		boolean flag = false;
+		try {
+			Connection connection = DBConnection.createConnection();
+			String sqlString="select * from seller where id=? and password=?";
+			PreparedStatement pst = connection.prepareStatement(sqlString);
+			pst.setInt(1, id);
+			pst.setString(2, op);
+			ResultSet rSet = pst.executeQuery();
+			if(rSet.next()) {
+				flag = true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return flag;
+	}
+	public static void changePasswrod(int id,String np) {
+		try {
+			Connection connection = DBConnection.createConnection();
+			String sqlString="update seller set password=? where id=?";
+			PreparedStatement pst = connection.prepareStatement(sqlString);
+			pst.setString(1, np);
+			pst.setInt(2, id);
+			pst.executeUpdate();
+			System.out.println("password changed");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
